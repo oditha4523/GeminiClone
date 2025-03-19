850ac49e14fea008461f43610345ac8d6030d595
oditha4523 <odithachamod@gmail.com>
2025-03-19 20:27:53 +0530

    first commit

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,81 @@
+import React, { useContext } from 'react'
+import './Main.css'
+import { assets } from '../../assets/assets'
+import { Context } from '../../context/context'
+
+const Main = () => {
+
+  const {onSent,recentPrompt,showResult,loading,resultData,setInput,input}= useContext(Context);
+
+  return (
+    <div className="main">
+        <div className="nav">
+            <p>Gemini</p>
+            <img src={assets.user_icon} alt="" />
+        </div>
+        <div className="main-container">
+
+            {!showResult 
+            ?<>
+                <div className="greet">
+                <p><span>Hello, Dev</span></p>
+                <p>How can I help you today</p>
+            </div>
+            <div className="cards">
+                <div className="card">
+                    <p>Suggests beautiful places to see on an upcoming road trip</p>
+                    <img src={assets.compass_icon} alt="" />
+                </div>
+                <div className="card">
+                    <p>Briefly summarize this content</p>
+                    <img src={assets.bulb_icon} alt="" />
+                </div>
+                <div className="card">
+                    <p>Breainstorm team bonding activities for our work retreat</p>
+                    <img src={assets.message_icon} alt="" />
+                </div>
+                <div className="card">
+                    <p>Improve the readability of the following code</p>
+                    <img src={assets.code_icon} alt="" />
+                </div>
+            </div>
+            </>
+            : <div className="result">
+                <div className="result-title">
+                    <img src={assets.user_icon} alt="" />
+                    <p>{recentPrompt}</p>
+                </div>
+                <div className="result-data">
+                    <img src={assets.gemini_icon} alt="" />
+                    {loading
+                    ?<div className='loader'>
+                        <hr />
+                        <hr />
+                        <hr />
+                    </div>
+                    :<p dangerouslySetInnerHTML={{__html:resultData}}></p>
+                    }
+                    
+                </div>
+            </div>
+            }
+
+            
+            <div className="main-bottom">
+                <div className="search-box">
+                    <input onChange={(e)=>setInput(e.target.value)} value={input} type="text" placeholder='Enter the promp here' />
+                    <div>
+                        <img src={assets.gallery_icon} alt="" />
+                        <img src={assets.mic_icon} alt="" />
+                        <img onClick={()=>onSent()} src={assets.send_icon} alt="" />
+                    </div>
+                </div>
+                <p className='bottom-info'>Build your own Generative AI App using Google Gemini API with React JS. 
+                Create AI App  like Google Gemini or ChatGPT step by step tutorial.</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Main
---
+++ src/config/gemini.js
@@ -0,0 +1,32 @@
+
+import {GoogleGenerativeAI} from "@google/generative-ai";
+  
+  const apiKey = "AIzaSyDHLvOZwuCfQi2BN7ssY7TJD9mO4foevS4";
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-2.0-flash",
+  });
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+  
+  async function runChat(prompt) {
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(prompt);
+    const response=result.response;
+    console.log(response.text());
+    return response.text();
+  }
+  
+  export default runChat;
