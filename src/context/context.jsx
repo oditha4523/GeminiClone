b2be7fea5b19806828801546209420f1cea0ce3b
oditha4523 <odithachamod@gmail.com>
2025-03-25 21:16:23 +0530

    adding new changes

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -15,7 +15,14 @@ const ContextProvider= (props)=>{
     const [resultData,setResultData] = useState("");
 
     const delayPara =(index,nextWord) =>{
+        setTimeout(function(){
+            setResultData(prev=>prev+nextWord);
+        },75*index)
+    }
 
+    const newChat=()=>{
+        setLoading(false);
+        setShowResult(false);
     }
 
     const onSent= async (prompt)=>{
@@ -23,10 +30,19 @@ const ContextProvider= (props)=>{
         setResultData("")
         setLoading(true)
         setShowResult(true)
-        setRecentPrompt(input)
-        const response = await runChat(input);
+        let response;
+        if(prompt!==undefined){
+            response=await runChat(prompt);
+            setRecentPrompt(prompt)
+
+        }else{
+            setPrevPrompts(prev=>[...prev,input]);
+            setRecentPrompt(input)
+            response=await runChat(input);
+        }
+
         let responseArray =response.split("**");
-        let newResponse;
+        let newResponse="";
         for(let i=0;i<responseArray.length;i++){
             if(i==0|| i%2 !==1){
                 newResponse+=responseArray[i];
@@ -37,7 +53,12 @@ const ContextProvider= (props)=>{
         }
 
         let newResponse2=newResponse.split("*").join("<br>")
-        setResultData(newResponse2);
+        let newResponseArray= newResponse2.split(" ");
+        for(let i=0;i<newResponseArray.length;i++){
+            const nextWord=newResponseArray[i];
+            delayPara(i,nextWord+" ");
+        }
+
         setLoading(false);
         setInput("");
     }
@@ -52,8 +73,8 @@ const ContextProvider= (props)=>{
         loading,
         resultData,
         input,
-        setInput
-        
+        setInput,
+        newChat
     }
 
     return(
